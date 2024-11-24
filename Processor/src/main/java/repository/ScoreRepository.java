1f6353b7274abb805ba7d8f9cc2ff91c3d8cd2fd
sm1853 <61781379+sm1853@users.noreply.github.com>
2024-11-24 18:43:04 -0500

    code commit

--- Processor/src/main/java/repository/ScoreRepository.java
+++
@@ -1,8 +0,0 @@
-package repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import entity.Score;
-
-public interface ScoreRepository extends JpaRepository<Score, Integer> {
-
-}
